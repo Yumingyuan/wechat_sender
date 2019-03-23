6cf0a4698105166850da84842187f91cc75efab0
Yumingyuan <timdavid@vip.qq.com>
2019-03-23 15:38:26 +0800

    删除Tinker

--- wechat_sender.py
+++ wechat_sender.py
@@ -7,25 +7,6 @@ from itchat.content import TEXT
 from itchat.content import *
 import time
 import os
-#from Tkinter import *
-#root=Tk()
-#listb_teacher=Listbox(root,selectmode=EXTENDED)
-#scroll_bar_teacher=Scrollbar(root)
-#side指定Scrollbar为居右；fill指定填充满整个剩余区域。
-#scroll_bar_teacher.pack(side=RIGHT,fill=Y)
-#下面的这句是关键：指定Listbox的yscrollbar的回调函数为Scrollbar的set
-#listb_teacher['yscrollcommand'] = scroll_bar_teacher.set
-#var=IntVar()
-#var.set(1)#单选互斥
-#Ra_Button1=Radiobutton(root, text="全选", variable=var, value=1)
-#Ra_Button1.pack( anchor = W )
-#Ra_Button2=Radiobutton(root, text="反选", variable=var, value=2)
-#Ra_Button2.pack( anchor = W )
-#def init_UI_teacher(list):
-#	for item in list:
-#		listb_teacher.insert(0,item['RemarkName'])
-#	listb_teacher.pack()
-#	root.mainloop()
 def send_to_all_teacher():
 	friends=itchat.get_friends(update=True)[0:]
 	teachers=[]
